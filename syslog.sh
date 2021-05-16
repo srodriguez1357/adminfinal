6af6f6708d20068e9a7257de43aef5ed1b5cb21e
Salvador Rodríguez <chavarrieta.2000@gmail.com>
2021-05-16 13:55:53 -0500

    segunda parte

--- syslog.sh
+++ syslog.sh
@@ -14,3 +14,14 @@ apt install php7.0 mcrypt -y
 apt install php7.0 php-mcrypt -y
 
 systemctl restart apache2
+
+cd /tmp
+wget http://download.adiscon.com/loganalyzer/loganalyzer-4.1.6.tar.gz
+tar -xzvf loganalyzer-4.1.6.tar.gz
+mkdir /var/www/html/loganalyzer
+cp -r /tmp/loganalyzer-4.1.6/src/* /var/www/html/loganalyzer
+cd /var/www/html/loganalyzer
+touch config.php
+chown www-data:www-data config.php
+chmod 666 config.php
+chown www-data:www-data -R /var/www/html/loganalyzer/
